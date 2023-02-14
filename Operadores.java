359852a3d9551fa1082b0bccbd3948ebc18abf14
Valvalz <bentogabryel048@gmail.com>
2023-02-14 14:31:02 +0000

    Salvar

--- Operadores.java
+++ Operadores.java
@@ -1,3 +1,16 @@
+import java.text.Format;
+
 public class Operadores {
+    
+    public static void main(String[] args){
+        int x = 9;
+        int y = 2;
+
+        System.out.print(Format:"x + y = %d\n" , x + y);
+        System.out.print(Format:"x - y = %d\n" , x - y);
+        System.out.print(Format:"x * y = %d\n" , x * y);
+        System.out.print(Format:"x / y = %d\n" , x / y);
+        System.out.print(Format:"x / y = %2.2d\n" , x /(double)y);
+        System.out.print(Format:"x %% y = %d\n" , x % y);
     
 }
