b3971ae4df21e18e6db8f94a60ea4bc05e98fade
Valvalz <bentogabryel048@gmail.com>
2023-02-28 15:18:10 +0000

    Salvar

---
+++ Salario.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Salario {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        double Salarioatual, Salarionovo;
+
+        System.out.println("Informe seu salario atual");
+        Salarioatual = input.nextFloat();
+
+       
+
+
+
+
+    
+}
+}
