42af445cb671ae2063fab49b64d4c2fe275008fb
Valvalz <bentogabryel048@gmail.com>
2023-03-06 11:21:43 +0000

    Salvar

--- Salario.java
+++ Salario.java
@@ -2,11 +2,28 @@ import java.util.Scanner;
 
 public class Salario {
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        double Salarioatual, Salarionovo;
 
-        System.out.println("Informe seu salario atual");
-        Salarioatual = input.nextFloat();
+        Scanner input = new Scanner(System.in);
+        double SalarioAtual, SalarioNovo;
+
+        System.out.println("Informe seu Salário Atual: "); 
+        SalarioAtual = input.nextDouble();
+
+        if (SalarioAtual > 1000 && SalarioAtual < 2000) {
+            SalarioNovo = SalarioAtual * 1.15 ;
+            System.out.println("Seu novo salário é:  \n" + SalarioNovo);
+            
+        }else if (SalarioAtual > 2000 && SalarioAtual < 3000) {
+            SalarioNovo = SalarioAtual * 1.10;
+            System.out.println("Seu novo salário é:  \n" + SalarioNovo);
+
+        }else if (SalarioAtual > 2000 && SalarioAtual < 3000) {
+            SalarioNovo = SalarioAtual * 1.05;
+            System.out.println("Seu novo salário é:  \n" + SalarioNovo);
+        }
+    }
+   
+        
 
        
 
@@ -14,5 +31,4 @@ public class Salario {
 
 
     
-}
 }
